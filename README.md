54eca4f31b519c442c9e16a12be00c1966dc30cc
Matthew Wei <matthewwei35@gmail.com>
2020-09-07 13:10:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CS-1.0_MW_Functions_Tutorial
+ Functions Tutorial assignment for CS 1.0
---
+++ functions.py
@@ -0,0 +1,34 @@
+#---- Q1 ----
+def area(width, height):
+    result = width * height
+    return result
+
+#---- Q2 ----
+result = area(5, 6)
+#print(result)
+
+#---- Q3 ----
+print(area(10, 20))
+print(area(2, 3))
+
+#---- Q4 ----
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+#---- Q5 ----
+print(subtract(5, 5))
+print(subtract(100, 50))
+
+#---- Q6 ----
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+#---- Q7 ----
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+print(add(6, 6))
+print(add(10, 10))
